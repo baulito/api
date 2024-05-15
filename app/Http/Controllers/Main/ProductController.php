7f128611fce3d490e8766702fa548bf5214325c1
Carlos rodriguez <alos1212@gmail.com>
2024-05-15 15:12:43 -0500

    ajustes

--- app/Http/Controllers/Main/ProductController.php
+++ app/Http/Controllers/Main/ProductController.php
@@ -31,6 +31,7 @@ class ProductController extends Controller
             $contents->where("state",'0')->where("amount","<",'1');
         } else if(isset($data['out'])) {
             $contents->where("state",'1');
+            $contents->where("amount","<>",'0');
         }
        
         
